cd773e41f929ec4c6e2bac1b08a77f710d2b466c
Andhitia Rama <andhitia.r@gmail.com>
2015-09-12 19:54:24 +0700

    Add (1) opnsynid_accounting_configuration, (2) opnsynid_cash_account_type, and (3) opnsynid_bank_account_type

---
+++ opnsynid_bank_account_type/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2009 P. Christeas <p_christ@hol.gr>. All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Bank account type definition',
+    'summary' : 'Define company bank account',
+    'category': 'Hidden',
+    'version': '1.0',
+    'author': 'OpenSynergy Indonesia',
+    'website': 'https://opensynergy-indonesia.com',
+    'description': """
+
+    """,
+    'depends': ['opnsynid_accounting_configuration'],
+    'demo': [],
+    'data': [   
+                'view/res_company_view.xml',
+               ],
+    'installable': True,
+    'images': [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
